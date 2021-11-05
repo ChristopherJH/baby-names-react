a2042aa154ab01de68121826b241cfee9163f53a
Christopher Hallett <chrisjameshal@gmail.com>
2021-11-05 16:06:20 +0000

    Displayed list of names

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,44 @@
-import { greet } from "./utils/greet";
+import babyNames from "./babyNamesData.json"
 
 function App(): JSX.Element {
-  return <h1>{greet("World")}</h1>;
+  return (
+    <BabyList />
+  );
+}
+
+function BabyList(): JSX.Element {
+  return (
+    <div>
+      <hr />
+      <section id="baby-names">
+        {babyNames.map(ObjectToBaby)}
+      </section>
+    </div>
+  );
+}
+
+function BabyCard(props: BabyProps): JSX.Element {
+  return (
+    <div id={props.name} className={props.sex}>
+      {props.name}
+    </div>
+  )
+}
+
+function ObjectToBaby(baby: BabyProps): JSX.Element {
+  return (
+    <BabyCard 
+      id = {baby.id}
+      name = {baby.name}
+      sex = {baby.sex}
+    />
+  );
+}
+
+interface BabyProps {
+  id: number;
+  name: string;
+  sex: string;
 }
 
 export default App;
